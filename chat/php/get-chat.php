96250b91773b9120ae20677753a9b288c1852596
saidatou <saidatoudondasse65@gmail.com>
2022-12-14 15:38:22 +0100

    fin

---
+++ chat/php/get-chat.php
@@ -0,0 +1,37 @@
+<?php 
+    session_start();
+    if(isset($_SESSION['unique_id'])){
+        include_once "config.php";
+        $outgoing_id = $_SESSION['unique_id'];
+        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+        $output = "";
+        $sql = "SELECT * FROM messages LEFT JOIN users ON users.unique_id = messages.destinateur
+                WHERE (destinateur = {$outgoing_id} AND expediteur = {$incoming_id})
+                OR (destinateur = {$incoming_id} AND expediteur = {$outgoing_id}) ORDER BY msg_id";
+        $query = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($query) > 0){
+            while($row = mysqli_fetch_assoc($query)){
+                if($row['destinateur'] === $outgoing_id){
+                    $output .= '<div class="chat outgoing">
+                                <div class="details">
+                                    <p>'. $row['msg'] .'</p>
+                                </div>
+                                </div>';
+                }else{
+                    $output .= '<div class="chat incoming">
+                                <img src="php/images/'.$row['img'].'" alt="">
+                                <div class="details">
+                                    <p>'. $row['msg'] .'</p>
+                                </div>
+                                </div>';
+                }
+            }
+        }else{
+            $output .= '<div class="text">No messages are available. Once you send message they will appear here.</div>';
+        }
+        echo $output;
+    }else{
+        header("location: ../login.php");
+    }
+
+?>
